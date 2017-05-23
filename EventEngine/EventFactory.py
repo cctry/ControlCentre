6a7e43033c7f6648e29889702ab14a7b8f6c82ac
cctry <csycfl@gmail.com>
2017-05-23 23:04:26 +0800

    factories

---
+++ EventEngine/EventFactory.py
@@ -0,0 +1,8 @@
+class EventFactory(object):#TODO
+    TYPE_OPERATION = 1
+    TYPE_INQUIRY = 2
+    def getEvent(type):
+        if type == self.TYPE_OPERATION:
+            pass
+        elif type == self.TYPE_INQUIRY:
+            pass
