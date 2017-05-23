0a1ee3b0b950e073dfcbafe9bb878aaa86e847e3
cctry <csycfl@gmail.com>
2017-05-23 22:21:46 +0800

    complete Event super class

---
+++ EventEngine/Event.py
@@ -0,0 +1,20 @@
+import time
+
+class Event(object):
+    
+    def __init__(self):
+        self.__id = hash(str(time.time()))
+        self.__timestamp = time.time()
+        self.__validity = True
+
+    def getId(self):
+        return self.__id
+    
+    def getTimestamp(self):
+        return self.__timestamp
+    
+    def isValid(self):
+        return self.__validity
+    
+    def setValidity(self, bool):
+        self.__validity = bool
