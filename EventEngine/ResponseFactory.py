403d177d0b877d7f6431a67178e907219d006000
cctry <csycfl@gmail.com>
2017-05-24 10:04:22 +0800

    complete response factory

---
+++ EventEngine/ResponseFactory.py
@@ -0,0 +1,9 @@
+import Response
+class ResponseFactory(object):
+    def prepareResponse():
+        response = Response()
+        self.__response = response
+    
+    def getResponse(handler):
+        self.__response.setHandler(handler)
+        return self.__response
