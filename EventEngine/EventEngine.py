2349d988b2bb931d699ac32393f7d4dc84f482e5
cctry <csycfl@gmail.com>
2017-05-29 01:24:30 +0800

    fix

--- EventEngine/EventEngine.py
+++ EventEngine/EventEngine.py
@@ -7,22 +7,22 @@ from ResponseProcessor import *
 class EventEngine(object):    
 
     def __init__(self):
-        self.--eventQueue = Queue()
-        self.--responseQueue = Queue()
-        self.--eventFactory = EventFactory(eventQueue)
-        self.--responseFactory = ResponseFactory(responseQueue)
-        self.--responseProcessor = ResponseProcessor(responseQueue)
-        self.--eventProcessor = EventProcessor(eventQueue)
-        self.--responseProcessor.Start()
-        self.--eventProcessor.Start()
+        self.__eventQueue = Queue()
+        self.__responseQueue = Queue()
+        self.__eventFactory = EventFactory(self.__eventQueue)
+        self.__responseFactory = ResponseFactory(self.__responseQueue)
+        self.__responseProcessor = ResponseProcessor(self.__responseQueue)
+        self.__eventProcessor = EventProcessor(self.__eventQueue)
+        self.__responseProcessor.Start()
+        self.__eventProcessor.Start()
 
     
-    def setData(data):
+    def setData(self, data):
         self.__eventFactory.prepareEvent()
         self.__eventFactory.addMsg(key = 'object', value = data['object'])
         self.__eventFactory.addMsg(key = 'intent', value = data['intent'])
         if data['extra'] != None:
-            self.__eventFactory.addMsg(dict = data['extra'])
+            self.__eventFactory.addMsg(Dict = data['extra'])
         self.__eventFactory.getEvent()# put event into event queue      
         
 
