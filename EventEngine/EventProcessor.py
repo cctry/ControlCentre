5447b23ae95ecc088aea4a6ea43362ee8b84cec4
cctry <csycfl@gmail.com>
2017-05-30 12:22:19 +0800

    basic function completed

--- EventEngine/EventProcessor.py
+++ EventEngine/EventProcessor.py
@@ -6,13 +6,14 @@ class EventProcessor(object):
 
     __objects = {'light':lightFunc, 'robot': None, 'AC': None}
 
-    def __init__(self, queue):
+    def __init__(self, queue, responseFactory):
         self.__eventQueue = queue
         self.__active = False# The trigger of the processor
         self.__thread = Thread(target=self.__Run)# Thread of the processor
         self.__scheduler = ScheduleManager()# Timing tasks manager
 
-    def Start(self):
+    def Start(self, callback):
+        self.__callback = callback
         # Start the processor
         self.__active = True
         # Start the thread
@@ -20,10 +21,11 @@ class EventProcessor(object):
         
     def __Run(self):
         # Run the processor
-        while self.__active == True:
+        while self.__active:
             # Fetch event from the queue
             event = self.__eventQueue.get()# Block if the queue is empty           
-            handler = self.classify(event)
+            handler = self.classify(event)# get the operation handler
+            self.__callback(handler)           
 
     def classify(self, event):
         obj = event.getMsg('object')
